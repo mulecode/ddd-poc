c7e672de19e2b55ec595ecde280f278cfad3a315
Rafael Mule <15720390+rafael-bm@users.noreply.github.com>
2025-03-08 22:47:13 +0000

    Add product catalog page with filtering and pagination functionality

--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -1,33 +1,43 @@
 "use client"
-import {useEffect, useState} from "react";
-import {useRouter} from "next/navigation";
-import AppTitle from "@/app/components/AppTitle";
+
 import AppButton from "@/app/components/AppButton";
-import AppTableNav from "@/app/components/AppTableNav";
 import Image from "next/image";
+import AppTitle from "@/app/components/AppTitle";
+import {useRouter} from "next/navigation";
+import {useEffect, useState} from "react";
+import AppTableData from "@/app/components/AppTableData";
 import AppFilterBar from "@/app/components/AppFilterBar";
 import {AppSelectItem} from "@/app/components/AppFormSelect";
-import AppTableData from "@/app/components/AppTableData";
 import AppTableSummary from "@/app/components/AppTableSummary";
+import AppTableNav from "@/app/components/AppTableNav";
 
-interface User {
+
+interface Product {
     id: string;
+    code: string;
+    manufacturer: string;
+    supplier: string;
+    brand: string;
     name: string;
-    email: string;
+    description: string;
+    category: string;
+    subCategory: string;
+    originCountryCode: string;
+    status: string;
 }
 
-interface UsersApiResponse {
-    users: User[];
+interface ProductsResponse {
+    products: Product[];
     page: number;
     totalPages: number;
     size: number;
     totalElements: number;
 }
 
-export default function UsersPage() {
+export default function ProductsPage() {
     const router = useRouter();
 
-    const [users, setUsers] = useState<User[]>([]);
+    const [products, setProducts] = useState<Product[]>([]);
     const [currentPage, setCurrentPage] = useState<number>(0);
     const [pageSize, setPageSize] = useState<number>(5);
     const [totalPages, setTotalPages] = useState<number>(1);
@@ -35,22 +45,29 @@ export default function UsersPage() {
     const [queryStrings, setQueryStrings] = useState<string>("");
 
     useEffect(() => {
-        console.log(`Fetching users... ${currentPage} ${pageSize} ${queryStrings}`);
-        fetch(`http://localhost:8080/app/users?page=${currentPage}&size=${pageSize}&${queryStrings}`)
+        console.log(`Fetching products... ${currentPage} ${pageSize} ${queryStrings}`);
+        fetch(`http://localhost:8080/app/products?page=${currentPage}&size=${pageSize}&${queryStrings}`)
             .then((response) => response.json())
-            .then((data: UsersApiResponse) => {
-                setUsers(data.users);
+            .then((data: ProductsResponse) => {
+                setProducts(data.products);
                 setTotalPages(data.totalPages);
                 setTotalElements(data.totalElements);
             })
             .catch((error) => console.error("Error fetching users:", error));
     }, [currentPage, pageSize, queryStrings]);
 
-
     const filters = [
         {id: "id", name: "Identification"},
+        {id: "manufacturer", name: "Manufacturer"},
+        {id: "supplier", name: "Supplier"},
+        {id: "code", name: "Code"},
+        {id: "brand", name: "Brand"},
         {id: "name", name: "Name"},
-        {id: "email", name: "Email"},
+        {id: "description", name: "Description"},
+        {id: "category", name: "Category"},
+        {id: "subCategory", name: "Sub Category"},
+        {id: "originCountryCode", name: "Country of Origin"},
+        {id: "status", name: "Status"},
     ];
 
     const handleSearch = (selectedFilters: AppSelectItem[]) => {
@@ -62,33 +79,31 @@ export default function UsersPage() {
     return (
         <div>
             <AppTitle
-                title="Users Management"
-                subTitle="List of all users"
-                backgroundImage="/dashboard/users.png"
+                title="Product Catalog"
+                subTitle="List of all products"
+                backgroundImage="/dashboard/products.png"
                 actionButton={
                     <AppButton
                         variant="primary"
-                        onClick={() => router.push("/users/new")}
+                        onClick={() => router.push("/products/new")}
                     >
                         <Image src="/plus.svg" alt="View" width={24} height={24}/>
-                        <span>Create New User</span>
+                        <span>Add New Product</span>
                     </AppButton>
                 }
             />
             <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 bg-white mt-6">
-                {/* List of Users */}
                 <AppFilterBar filters={filters}
                               defaultFilter="name"
                               onSearch={handleSearch}/>
-                {/* List of Users */}
                 <AppTableData
-                    headers={["ID", "Name", "Email"]}
-                    columnWidths={["w-2/6", "w-2/6", "w-2/6"]}
-                    columnCss={["text-xs", "", ""]}
-                    data={users}
+                    headers={["ID", "Code", "Brand", "Name", "Status"]}
+                    columnWidths={["w-2/10", "w-2/10", "w-2/10", "w-2/10", "w-2/10"]}
+                    columnCss={["text-xs", "text-xs", "", "", ""]}
+                    data={products}
                     linkBaseUrl="/users"
                 />
-                {/* Tables footer */}
+
                 <div className="flex flex-wrap gap-4 w-full text-white">
                     {/* Pagination Summary */}
                     <AppTableSummary
@@ -105,8 +120,8 @@ export default function UsersPage() {
                         />
                     </div>
                 </div>
+
             </main>
         </div>
-    );
+    )
 }
-
