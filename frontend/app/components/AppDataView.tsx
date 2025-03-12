dcc3f9367103b0d179ef91fbf9b8141bd36e8fdb
Rafael Mule <15720390+rafael-bm@users.noreply.github.com>
2025-03-12 21:03:14 +0000

    Enhance grid layout to support additional columns for improved responsiveness

--- frontend/app/components/AppDataView.tsx
+++ frontend/app/components/AppDataView.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const AppDataView: React.FC<Props> = ({children}) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-8 gap-y-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-4">
             {children}
         </div>
     );
